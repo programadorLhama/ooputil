82cdc0fc23b15298d0cc951130a6ab93cfd50901
Rafael Ferreira <programadorlhama@gmail.com>
2025-03-08 20:08:54 -0300

    config: updating version and adding requirements.txt to project

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ooputil",
-    version="0.1.4",
+    version="0.2.4",
     description="Library for OOP utilities",
     author="Rafael da Rocha Ferreira",
     author_email="programadorlhama@gmail.com",
