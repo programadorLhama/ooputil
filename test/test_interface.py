532c2890d7273824afbea634aa81643669263afc
Rafael Ferreira <programadorlhama@gmail.com>
2025-03-08 19:56:14 -0300

    test: Adding signatures test

--- test/test_interface.py
+++ test/test_interface.py
@@ -12,7 +12,7 @@ def test_interface_ok():
     foo.do_something("Test OK! :)")
     assert True
 
-def test_interface_raise():
+def test_interface_raise_if_method_not_equal():
     class FooInterface(Interface):
         def do_something(self, elem: str) -> None: pass
 
@@ -23,3 +23,18 @@ def test_interface_raise():
     except Exception as e:
         assert str(e) == "Class 'Foo' must implement method 'do_something' of interface 'FooInterface'"
         assert True
+
+def test_interface_raise_if_signature_not_equal():
+    class FooInterface(Interface):
+        def do_something(self, elem: str) -> None: pass
+
+    try:
+        class Foo(FooInterface):
+            def do_something(self, elem: int) -> None:
+                print(elem)
+    except Exception as e:
+        assert "Method 'do_something' in class 'Foo' does not match the interface signature" in str(e)
+        assert "Expected: (self, elem: str) -> None" in str(e)
+        assert "Got: (self, elem: int) -> None" in str(e)
+        assert True
+
